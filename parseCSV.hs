7438a3086536b1a304b7b1bc3aa3c9b2a4e58418
Neuromancer42 <chenyifan_1997@hotmail.com>
2017-02-23 13:26:06 +0000

    compile CSV parser

--- parseCSV.hs
+++ parseCSV.hs
@@ -1,10 +1,28 @@
 import ParserCombinators
+import System.Environment
+import System.Exit
+import System.IO
 
+parseCSV :: Parser [[String]]
 parseCSV = many parseLine
   where
     parseLine = parseCell `sepBy` char ',' <* char '\n'
     parseCell = do
-        char '"'
-        content <- many (anyCharBut '"')
-        char '"'
-        return content
+      char '"'
+      content <- many (anyCharBut '"')
+      char '"'
+      return content
+
+main :: IO ()
+main = do
+  args <- getArgs
+  input <-
+    case args of
+      [] -> getContents
+      [fileName] -> readFile fileName
+      _ -> hPutStrLn stderr "Too many arguments" >> exitFailure
+  case parse parseCSV input of
+    [] -> do
+      hPutStrLn stderr "Failed to parse INI file."
+      exitFailure
+    x:_ -> print x
